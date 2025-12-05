588b8380e11ba5f9549b498b4c9125e60ee85b9f
Daniel Igaz <daniel.igaz@externals.hilti.com>
2025-12-05 23:55:24 +0100

    Finished day2.

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashSet,
     fs::File,
     io::{self, BufRead},
     usize,
@@ -48,7 +49,40 @@ fn part1() -> usize {
     acc
 }
 
+fn part2() -> usize {
+    let mut invalid_ids = HashSet::new();
+
+    for (start, end) in get_ranges() {
+        for value in start..=end {
+            let digits = value.to_string().len();
+
+            for decimals in 1..=digits / 2 {
+                // get the possibile divisors
+                if digits % decimals == 0 {
+                    let offset = 10usize.pow(decimals as u32);
+                    let sequence = value % offset;
+
+                    // create pattern based on repeating sequence
+                    let mut pattern = 0;
+                    for _ in 1..=digits / decimals {
+                        pattern *= offset;
+                        pattern += sequence;
+                    }
+
+                    // match the pattern
+                    if value == pattern {
+                        invalid_ids.insert(value);
+                    }
+                }
+            }
+        }
+    }
+
+    invalid_ids.into_iter().sum()
+}
+
 fn main() -> io::Result<()> {
     println!("Part1: invalid IDs sum = {:?}", part1());
+    println!("Part2: invalid IDs sum = {:?}", part2());
     Ok(())
 }
